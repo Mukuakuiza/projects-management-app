d359ca91c3b90ad39ec8cd84c49e942b74a199cf
Mukuakuiza <hinomukuakuiza12@gmail.com>
2024-12-10 03:15:45 +0200

    getting user input and show the project in the sidebar

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,11 +1,33 @@
 import Input from "./Input.jsx";
+import { useRef } from "react";
+const NewProject = ({ onAdd }) => {
+  const titleRef = useRef();
+  const startDateRef = useRef();
+  const dueDateRef = useRef();
+  const descriptionRef = useRef();
 
-const NewProject = () => {
+  const handleSaveInput = () => {
+    const enteredTitle = titleRef.current.value;
+    const enteredStartDate = startDateRef.current.value;
+    const enteredDueDate = dueDateRef.current.value;
+    const enteredDescription = descriptionRef.current.value;
+
+    onAdd({
+      title: enteredTitle,
+      startDate: enteredStartDate,
+      dueDate: enteredDueDate,
+      description: enteredDescription,
+    });
+  };
+
   return (
     <div className="w-[35rem] mt-16 ">
       <menu className="flex items-center justify-end gap-4 my-4">
         <li>
-          <button className="bg-stone-900 text-white px-7 py-2 rounded-md hover:bg-stone-600 hover:text-white">
+          <button
+            onClick={handleSaveInput}
+            className="bg-stone-900 text-white px-7 py-2 rounded-md hover:bg-stone-600 hover:text-white"
+          >
             Save
           </button>
         </li>
@@ -16,10 +38,10 @@ const NewProject = () => {
         </li>
       </menu>
       <div>
-        <Input label="Title" />
-        <Input label="Start Date" />
-        <Input label="Due Date" />
-        <Input label="Description" isTextarea />
+        <Input type="text" label="Title" ref={titleRef} />
+        <Input type="date" label="Start Date" ref={startDateRef} />
+        <Input type="date" label="Due Date" ref={dueDateRef} />
+        <Input label="Description" isTextarea ref={descriptionRef} />
       </div>
     </div>
   );
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,6 +1,7 @@
 import Button from "./Button.jsx";
 
-const SideBar = ({ startAddProject }) => {
+const SideBar = ({ startAddProject, projects }) => {
+  console.log(projects);
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200 ">
@@ -10,7 +11,15 @@ const SideBar = ({ startAddProject }) => {
       <div>
         <Button onClick={startAddProject}>Create Project</Button>
       </div>
-      <ul></ul>
+      <ul className="mt-8">
+        {projects.map((project) => (
+          <li key={project.id}>
+            <button className="w-full text-left px-2 py-2 text-sm rounded-md text-stone-400 hover:bg-stone-700 hover:text-stone-200">
+              {project.title}
+            </button>
+          </li>
+        ))}
+      </ul>
     </aside>
   );
 };
